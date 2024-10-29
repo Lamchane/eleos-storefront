44d4225c08e527dfa60fd3833414ac5b1976a43f
nathgoutam93 <nathgoutam93@gmail.com>
2024-10-29 20:24:33 +0530

    featuredCategories

---
+++ src/modules/home/components/featured-products/category-tabs/index.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import React, { Suspense } from "react"
+import {
+  ProductCategoryWithPreviews,
+  ProductCollectionWithPreviews,
+} from "types/global"
+
+import { Tabs } from "@medusajs/ui"
+import { Region } from "@medusajs/medusa"
+
+import ProductRail from "@modules/home/components/featured-products/product-rail"
+// import Tabs from "../tab"
+
+type Props = {
+  categoryWithProducts: ProductCategoryWithPreviews[]
+  region: Region
+}
+
+function CategoryWithProductRail({ categoryWithProducts, region }: Props) {
+  // const tabsData = collectionWithProducts.map((collection) => ({
+  //   label: collection.title,
+  //   content: (
+  //     <ProductRail
+  //       handle={collection.handle ?? ""}
+  //       products={collection.products ?? []}
+  //       region={region}
+  //     />
+  //   ),
+  // }))
+
+  return (
+    <div className="w-full">
+      {categoryWithProducts.length > 0 && (
+        // @ts-expect-error
+        <Tabs defaultValue={categoryWithProducts[0].id}>
+          <Tabs.List className="flex justify-center">
+            {categoryWithProducts.map((category) => (
+              <Tabs.Trigger key={category.id} value={category.id}>
+                {category.name}
+              </Tabs.Trigger>
+            ))}
+          </Tabs.List>
+          <div className="mt-2">
+            {categoryWithProducts.map((category) => (
+              <Tabs.Content key={category.id} value={category.id}>
+                <ProductRail
+                  handle={category.handle ?? ""}
+                  products={category.products ?? []}
+                  region={region}
+                />
+              </Tabs.Content>
+            ))}
+          </div>
+        </Tabs>
+      )}
+    </div>
+  )
+}
+
+export default CategoryWithProductRail
--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -1,10 +1,21 @@
 import { cache } from "react"
-import { Product, Region, ProductCollection } from "@medusajs/medusa"
+import {
+  Product,
+  Region,
+  ProductCollection,
+  ProductCategory,
+} from "@medusajs/medusa"
 
 import { getProductsList, getRegion } from "@lib/data"
 
-import { ProductCollectionWithPreviews } from "types/global"
+import {
+  ProductCategoryWithChildren,
+  ProductCategoryWithPreviews,
+  ProductCollectionWithPreviews,
+} from "types/global"
+
 import CollectionWithProductRail from "./collection-tabs"
+import CategoryWithProductRail from "./category-tabs"
 
 const getCollectionsWithProducts = cache(
   async (
@@ -46,7 +57,47 @@ const getCollectionsWithProducts = cache(
   }
 )
 
-export default async function FeaturedProducts({
+const getCategoriesWithProducts = cache(
+  async (
+    categories: ProductCategoryWithChildren[],
+    countryCode: string
+  ): Promise<ProductCategoryWithPreviews[] | null> => {
+    if (!categories) {
+      return null
+    }
+
+    const categoriesIds = categories.map((category) => category.id)
+
+    await Promise.all(
+      categoriesIds.map((id) =>
+        getProductsList({
+          queryParams: { category_id: [id], limit: 5 },
+          countryCode,
+        })
+      )
+    ).then((responses) =>
+      responses.forEach(({ response, queryParams }) => {
+        let category
+
+        if (categories) {
+          category = categories.find(
+            (category) => category.id === queryParams?.category_id?.[0]
+          )
+        }
+
+        if (!category) {
+          return
+        }
+
+        category.products = response.products as unknown as Product[]
+      })
+    )
+
+    return categories as unknown as ProductCategoryWithPreviews[]
+  }
+)
+
+export async function FeaturedCollections({
   collections,
   countryCode,
 }: {
@@ -64,6 +115,32 @@ export default async function FeaturedProducts({
     <section className="py-12">
       <CollectionWithProductRail
         collectionWithProducts={collectionWithProducts ?? []}
+        region={region as Region}
+      />
+    </section>
+  )
+}
+
+export async function FeaturedCategories({
+  categories,
+  countryCode,
+}: {
+  categories: ProductCategoryWithChildren[]
+  countryCode: string
+}) {
+  const region = await getRegion(countryCode)
+
+  const categoryWithProducts = await getCategoriesWithProducts(
+    categories,
+    countryCode
+  )
+
+  console.log(categoryWithProducts)
+
+  return (
+    <section className="py-12">
+      <CategoryWithProductRail
+        categoryWithProducts={categoryWithProducts ?? []}
         region={region as Region}
       />
     </section>
