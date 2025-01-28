addadf254aea5403a232bcbb7065f72eac5dec53
nathgoutam93 <nathgoutam93@gmail.com>
2025-01-28 17:39:38 +0530

    restyle coupn

--- src/modules/home/components/collection-swiper/index.tsx
+++ src/modules/home/components/collection-swiper/index.tsx
@@ -50,9 +50,9 @@ function CollectionSwiper({ collections }: Props) {
         <LocalizedClientLink href={`/store`}>
           <Image
             src={
-              "https://eleos-medusa-uploads.s3.ap-south-1.amazonaws.com/Hero_Images/ELEOS+ny+wthout.png"
+              "https://eleos-medusa-uploads.s3.ap-south-1.amazonaws.com/Hero_Images/222.webp"
             }
-            alt={`new year sale`}
+            alt={`celebrating love`}
             width={1920}
             height={1080}
           />
