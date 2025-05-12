003d1691978c2050e27d070d041f701abc81013b
nathgoutam93 <nathgoutam93@gmail.com>
2025-05-12 16:44:21 +0530

    hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,21 +39,6 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       <body>
         <main className="relative">{props.children}</main>
 
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-        window.omnisend = window.omnisend || [];
-        omnisend.push(["brandID", "67f39b3622cb9bfb8c05e3fd"]);
-        omnisend.push(["track", "$pageViewed"]);
-        !function(){var e=document.createElement("script");
-        e.type="text/javascript",e.async=!0,
-        e.src="https://omnisnippet1.com/inshop/launcher-v2.js";
-        var t=document.getElementsByTagName("script")[0];
-        t.parentNode.insertBefore(e,t)}();
-        `,
-          }}
-        />
-
         {/* PixelPageView dynamically imported to avoid SSR issues */}
         <PixelPageView />
         <ClarityInitializer />
